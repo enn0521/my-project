696abb29b946bb0d84693087643f39e84b4c6ede
enn0521 <user106214@gmail.com>
2025-09-25 11:48:25 +0800

    update

--- my-project/sqlite.py
+++ my-project/sqlite.py
@@ -1,8 +1,15 @@
 import sqlite3
-conn = sqlite3.connect('product_info.db')
+conn = sqlite3.connect('..\lesson_sql\product_info.db')
 
 cursor = conn.cursor()
 cursor.execute('SELECT * FROM product_info')
 
+sqlins = """
+SELECT id, name FROM product_info 
+WHERE id = "1"
+"""
+
+cursor.execute(sqlins)
 records = cursor.fetchall()
 print(records)
+
